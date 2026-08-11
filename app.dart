e6122125908d1d455cf573e700a7bb88bc12acb7
marziarezayee11 <mandgarmarziaroya@gmail.com>
2026-08-11 20:35:35 +0430

    first commit

---
+++ app.dart
@@ -0,0 +1,22 @@
+void main(){
+    num num1 = 3;
+    num num2 = 4;
+    print(num1>num2);
+       
+       num score = 90;
+       if(score <=100 &&  score >=  90){
+        print("you are in a group");
+       }
+       else if(score >= 80){
+        print("you are in b group");
+       }
+        else if(score >= 60){
+        print("you are in c group");
+       }
+       else{
+        print("you are in f group");
+       };
+      num1 += num2;
+      print(num1);
+     
+}
