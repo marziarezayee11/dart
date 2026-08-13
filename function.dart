d05215b52c9df9f1f281461cee1ac154b59e0e9c
marziarezayee11 <mandgarmarziaroya@gmail.com>
2026-08-13 22:35:55 +0430

    loop

---
+++ function.dart
@@ -0,0 +1,24 @@
+ greeting(){
+    print("hi dears");
+}
+String ali(){
+    return"hi ali";
+}
+int sum(int a,int b){
+    return  a+b;
+}
+String function(){
+    return "fetching the best server";
+}
+void main(){
+    greeting();
+String result = ali();
+print(result);
+ int resum = sum(60,70);
+ print(resum);
+ int resu = sum(80,100);
+ print(resu);
+String func = function();
+print(func);
+
+}
