dc9109b384731d79eb342e59c5c64f77ee5cb4a1
marziarezayee11 <mandgarmarziaroya@gmail.com>
2026-08-16 19:30:06 +0430

    loops

--- loop.dart
+++ loop.dart
@@ -27,4 +27,29 @@ print(num1 += a);
         }
         print(i);
     }
+    // marzee
+    for (int i = 0 ; i<=100; i++){
+        if(i%7 == 0){
+            continue;
+        }
+        print(i);
+    }
+    //end
+   for(int b = 1; b<=100; b++){
+    if(b%7 ==0){
+        continue;
+    }
+    print(b);
+   }
+
+   List<int>scores = [56,20,60,70, 48, 89,];
+   for(int i = 0 ; i<scores.lenght; i++){
+    if(scores[i]<50){
+        print("there st withe ID ${i+1}you should try ");
+    }
+    if(scores[i]>=60){
+        print("there st withe ID ${i+1}congratulation you got ${scores}");
+
+    }
+   }
 }
