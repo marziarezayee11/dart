e2bc48d583ca35f839f89894dd22ab466365ecd2
marziarezayee11 <mandgarmarziaroya@gmail.com>
2026-08-21 21:22:48 +0430

    length

--- ListMP.dart
+++ ListMP.dart
@@ -40,10 +40,10 @@ void main() {
   List<num> prices = [230, 340, 560, 600, 690, 670, 540];
   List<num> curretP = prices.map((price) => price - (price * 5) / 100).toList();
   print(curretP);
-  // List<num> salaries = [12000, 14000, 18000, 20000];
-  // List<num> salariesAfter = salaries
-  //     .map((salari) => salari + (salari * 6) / 100)
-  //     .toList();
-  // print("salaries after adding the bonus gose like this$salariesAfter");
-  
+  List<num> salaries = [12000, 14000, 18000, 20000];
+  List<num> salariesAfter = salaries
+      .map((salari) => salari + (salari * 6) / 100)
+      .toList();
+  print("salaries after adding the bonus gose like this$salariesAfter");
+  print(prices.length);
 }
